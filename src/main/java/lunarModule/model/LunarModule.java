e9ba2b512291e9f7d14f6522c28472109b11df86
Renato Lopes <renato343@gmail.com>
2017-03-30 23:10:30 +0100

    parser in lunar module done

--- src/main/java/lunarModule/model/LunarModule.java
+++ src/main/java/lunarModule/model/LunarModule.java
@@ -24,7 +24,7 @@ public class LunarModule {
 
         try {
 
-            houstonSocket = new Socket(name,port);
+            houstonSocket = new Socket(name, port);
 
             LunarListener sender = new LunarListener(houstonSocket);
             LunarSender listener = new LunarSender(houstonSocket);
@@ -45,6 +45,8 @@ public class LunarModule {
 
     private class LunarListener implements Runnable {
 
+        String messageIn;
+
         public LunarListener(Socket houston) {
 
             houstonSocket = houston;
@@ -62,7 +64,7 @@ public class LunarModule {
             try {
 
 
-               DataInputStream in = new DataInputStream(houstonSocket.getInputStream());
+                DataInputStream in = new DataInputStream(houstonSocket.getInputStream());
 
                 while (!houstonSocket.isClosed()) {
 
@@ -70,9 +72,10 @@ public class LunarModule {
 
                     System.out.println("waiting Houston to write");
 
-                    String messageIn = in.readLine();
+                    messageIn = in.readLine();
 
-                    System.out.println(messageIn);
+                    parserThrust(messageIn);
+                    parserDirection(messageIn);
 
                     //console.setText(messageIn);
 
@@ -86,14 +89,29 @@ public class LunarModule {
                 e.printStackTrace();
             }
         }
+
+        public void parserDirection(String messageReceived) {
+
+            System.out.println("this is message Received " + messageReceived);
+            String direction = messageReceived.split(" ")[0];
+            System.out.println("this is direction " + direction);
+
+        }
+
+        public void parserThrust(String messageReceived) {
+
+            System.out.println("this is message Received  in Thrust " + messageReceived);
+            int thrust = Integer.parseInt(messageReceived.split(" ")[1]);
+            System.out.println("this is thrust " + thrust);
+
+        }
     }
 
     private class LunarSender implements Runnable {
 
 
-
         private LunarSender(Socket houston) {
-           houstonSocket = houston;
+            houstonSocket = houston;
         }
 
         public synchronized void run() {
@@ -136,7 +154,7 @@ public class LunarModule {
 
     public static void main(String[] args) {
 
-        LunarModule lunarModule = new LunarModule("localhost",8080);
+        LunarModule lunarModule = new LunarModule("localhost", 8080);
 
         lunarModule.start();
 
